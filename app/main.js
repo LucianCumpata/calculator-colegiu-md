30c7b20a5a5831ac9665258d7e8220de12254f62
Lucian Cumpata <cumpata.lu@outlook.com>
2018-12-25 21:18:15 +0200

    added new input field

--- app/main.js
+++ app/main.js
@@ -11,15 +11,25 @@ function calculeazaMedia() {
     let frst_field = document.getElementById("1n").value;
     let scnd_field = document.getElementById("2n").value;
     let result = ((frst_field*0.6)+(scnd_field*0.4));
-    document.getElementById("rezultat").innerHTML = result;
+    let answer = "Nota finala este: ";
+    document.getElementById("rezultat").innerHTML = answer + result;
     //return (a*0.6)+(b*0.4);
 }
 
 function calculeazaMediaCurenta() {
-    let frst_field = document.getElementById("c1n").value;
-    let scnd_field = document.getElementById("c2n").value;
-    let result = ((frst_field * 1)+(scnd_field * 1))/2;
-    document.getElementById("rezultat_medie_curenta").innerHTML = result;
+    let frst_field = document.getElementById("t1n").value;
+    let scnd_field = document.getElementById("si2n").value;
+    let thrd_field = document.getElementById("pr2n").value;
+    
+    let answer = "Media curenta este: ";
+    let result = 0; 
+
+    if (thrd_field == "" || thrd_field == 0){
+        result = ((frst_field * 1)+(scnd_field * 1))/2;
+    } else {
+        result = ((frst_field * 1)+(scnd_field * 1)+(thrd_field * 1))/3;
+    }
+    document.getElementById("rezultat_medie_curenta").innerHTML = answer + result;
     //return ;
 }
 
